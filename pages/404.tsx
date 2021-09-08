48db0c831241a5a136a1f311472c35237803df5e
Nick Roberts <djparadyme@gmail.com>
2021-09-08 17:32:58 -0400

    Minor page updates

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,37 @@
 import Link from 'next/link';
+import Page from '@components/page';
 
 export default function FourOhFour() {
+  const meta = {
+    title: 'UTROPICMEDIA | Page not found',
+    description: `We couldn't find the page you're looking for.`
+  };
   return (
-    <div className="bg-gray-900 text-gray-50">
-      <h1>404 - Page Not Found</h1>
-      <Link href="/">
-        <a>Go back home</a>
-      </Link>
-    </div>
+    <Page meta={meta} fullViewport>
+      <div className="min-h-screen pt-16 pb-16 flex flex-col">
+        <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="py-16">
+            <div className="text-center">
+              <p className="text-sm font-semibold text-gray-50 uppercase tracking-wide">
+                404 error
+              </p>
+              <h1 className="mt-2 text-4xl font-extrabold tracking-tight sm:text-5xl">
+                Page not found.
+              </h1>
+              <p className="mt-2 text-base text-gray-200">
+                Sorry, we couldn’t find the page you’re looking for.
+              </p>
+              <div className="mt-6">
+                <Link href="/">
+                  <a className="text-base font-medium text-gray-50 hover:text-gray-200">
+                    Go back home<span aria-hidden="true"> &rarr;</span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </Page>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@ import Page from '@components/page';
 
 export default function Home() {
   const meta = {
-    title: 'Utropicmedia',
-    description: 'TODO!'
+    title: 'UTROPICMEDIA | Awesome Development',
+    description: `A collective of premier technologists with a passion for solution development, focusing on Infrastructure, Security, Data Distribution and Managed SaaS Solutions. Members of Utropicmedia focus on understanding new technology and its impact on industry and consumers. Members are encouraged to contribute knowledge and build discussion with the community at-large with the goal of advancing technology's effectiveness, value and simplicity.`
   };
   return (
     <Page meta={meta} fullViewport>
-      <h1>Utropicmedia</h1>
+      <div className="min-h-screen pt-16 pb-16 flex flex-col">
+        <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">Utropicmedia</h1>
+          </div>
+        </main>
+      </div>
     </Page>
   );
 }
